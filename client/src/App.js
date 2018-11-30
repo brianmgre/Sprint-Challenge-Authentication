7329d71ef3f66215dbf0e1be33509bbbd9e9219d
Brian G <brian.gregorius@gmail.com>
2018-11-30 12:25:02 -0500

    completed setup of react-app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-
+import SignUp from './components/SignUp.js';
+import SignIn from './components/SignIn.js';
+import Jokes from './components/Jokes.js';
+import { Switch, Route, NavLink } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-     
+        <nav>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/signup'>Sign Up</NavLink>
+          <NavLink to='/signin'> Sign In</NavLink>
+        </nav>
+        <h1>Wanna Hear Some Jokes? Sign In|Up</h1>
+        <section>
+          <Switch>
+            <Route path='/signup' component={SignUp} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/jokes' component={Jokes} />
+          </Switch>
+        </section>
       </div>
     );
   }
---
+++ client/src/components/Jokes.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+class Jokes extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    };
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+
+
+
+export default Jokes;
